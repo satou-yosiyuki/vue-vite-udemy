7e80399668b7ab594e2e892862cacc7cec675e02
satou-yosiyuki <stuysyk1027@icloud.com>
2022-04-08 22:33:32 +0900

    ref,reactive

--- src/views/CompositionTest.vue
+++ src/views/CompositionTest.vue
@@ -1,8 +1,19 @@
 <template>
   <p>CompositionTest</p>
+  <p>{{ name }}</p>
+  <p>{{ nameRef }}</p>
+  <p>{{ book.title }}</p>
+  <p>{{ book.author[0] }}</p>
 </template>
 
-<script>
+<script setup>
+import { ref, reactive } from "vue";
+const nameRef = ref("あああ");
+const book = reactive({
+  title: "タイトル",
+  author: ["大谷"],
+});
+const name = "佐藤";
 </script>
 
 <style></style>
