3ac69e7915d7d1c2a26004cc30f5169d69a3b3dd
satou-yosiyuki <stuysyk1027@icloud.com>
2022-04-08 23:05:58 +0900

    watch.computed

--- src/views/CompositionTest.vue
+++ src/views/CompositionTest.vue
@@ -4,15 +4,43 @@
   <p>{{ nameRef }}</p>
   <p>{{ book.title }}</p>
   <p>{{ book.author[0] }}</p>
+  <p>{{ totalPrice }}</p>
+  <div><input v-model="search" /></div>
+  <div><input v-model="searchEffect" /></div>
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed, watch, watchEffect, onMounted } from "vue";
 const nameRef = ref("あああ");
 const book = reactive({
   title: "タイトル",
-  author: ["大谷"],
+  author: ["大谷2", "伊藤2"],
 });
+
+const item = reactive({
+  price: 100,
+  number: 1,
+});
+
+const totalPrice = computed(() => {
+  return item.price * item.price;
+});
+
+const search = ref("");
+watch(search, (newValue, prevValue) => {
+  console.log(`watch: ${search.value}`);
+  console.log(`new: ${newValue}`);
+  console.log(`prev: ${prevValue}`);
+});
+const searchEffect = ref("");
+watchEffect(() => {
+  console.log(`watchEffect: ${searchEffect.value}`);
+});
+
+onMounted(() => {
+  console.log("onMounted");
+});
+
 const name = "佐藤";
 </script>
 
